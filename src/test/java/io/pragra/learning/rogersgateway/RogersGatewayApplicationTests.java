6155d1c72d05e903927a57e106c841531b4e89eb
pragradev <shubh@pragra.io>
2024-11-27 05:38:07 +0400

    first commit

---
+++ src/test/java/io/pragra/learning/rogersgateway/RogersGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pragra.learning.rogersgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RogersGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
